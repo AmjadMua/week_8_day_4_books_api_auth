d251021ee5e2740c9d4cea65e7d34c5e7196d730
AmjadMua <92171759+AmjadMua@users.noreply.github.com>
2021-12-05 05:55:26 +0300

    cors installed.

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const cors = require("cors")
 require("dotenv").config()
 const books = require("./routes/books")
 const users = require("./routes/users")
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 mongoose
-  .connect(
-    `mongodb+srv://amjadill:${process.env.MONGODB_PASSWORD}@cluster0.j6bza.mongodb.net/day4Database?retryWrites=true&w=majority`
-  )
+  .connect(`mongodb://localhost:27017/testDB`)
   .then(() => console.log("connected to database"))
   .catch(error => console.log("ERROR", error))
 
 app.use("/api/books", books)
 app.use("/api/users", users)
 
-app.listen(3000, () => {
-  console.log("port is listening now!", 3000)
+app.listen(5000, () => {
+  console.log("port is listening now!", 5000)
 })
+
+//                    for react
+//              http://localhost:5000/api/books
+//              http://localhost:5000/api/users
